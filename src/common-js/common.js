3bf8abc2719875584b2543f6dce49bd9b25be581
danichagas <chagasdaniel788@gmail.com>
2024-10-12 15:54:57 -0300

    Common JS

--- src/common-js/common.js
+++ src/common-js/common.js
@@ -1,7 +1,12 @@
 const p = require('./services/products')
-
+const config = require('./services/config')
+const database = require('./services/database')
 async function main() {
-  p.getFullName(324, 'teclado gamer') 
+  p.getFullName(324, 'Cadeira ergonômica')
+  p.getProductName('Cadeira ergonômica')
+
+  console.log(config.version)
+  database.connectToDatabase('MongoDB')
 }
 
 main()
