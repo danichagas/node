3cea8ebda7b050d53b0ee0921679bb229f70810d
danichagas <chagasdaniel788@gmail.com>
2024-10-13 13:47:31 -0300

    Multiple exports

--- src/esm-js/utils/database.js
+++ src/esm-js/utils/database.js
@@ -2,4 +2,11 @@ async function connectToDatabase(dataName) {
   console.log(`Conectado ao banco ${dataName}!`)
 }
 
-export default connectToDatabase
+async function disconnectDatabase() {
+  console.log('Desconectando do banco de dados!')
+}
+
+export {
+  connectToDatabase,
+  disconnectDatabase
+}  
