d7c1d18691833fd77dcbdb4ff70697e8ada0a65f
danichagas <chagasdaniel788@gmail.com>
2024-10-13 14:16:41 -0300

    Ecmascript Modules

--- src/esm-js/esm.js
+++ src/esm-js/esm.js
@@ -1,5 +1,7 @@
 import * as database from './utils/database.js'
+import { getDataApi } from './utils/api.js'
 
 database.connectToDatabase('PostgreSQL')
 database.disconnectDatabase()
 console.log(database.databaseType)
+getDataApi()
